7561363d2d0fae5665bfd9d968c9faebc7753c44
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-08-05 23:45:40 +0530

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Counter extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            count:0,
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h1>Counter App</h1>
+                <p>Counter: {this.state.count}</p>
+                <button onClick={()=>this.setState({count:this.state.count+1})}>Increment</button>
+                <button onClick={()=>this.setState({count:this.state.count-1})}>Decrement</button>
+            </div>
+        )
+    }
+}
+export default Counter;
